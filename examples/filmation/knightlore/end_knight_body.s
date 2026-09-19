223ad20bbc78f0f5e9063a381c82cfa820d368d1
Jon Sole <15461752+jonsole@users.noreply.github.com>
2026-09-19 12:44:03 +0100

    Revert "Knight Lore game over: the knight standing under the words, in white"
    This reverts commit dadc2dce0e929c7769d3f89305ea8e8398f26f52.

--- examples/filmation/knightlore/end_knight_body.s
+++
@@ -1,7 +0,0 @@
-; The knight's body on the game-over screen -- see end_knight.s.
-; Corrupts everything.
-end_knight_body:    ld      a,40
-                    ld      c,END_KNIGHT_X
-                    ld      de,END_KNIGHT_FEET - 8
-                    call    screen_sprite
-                    jp      end_rating
